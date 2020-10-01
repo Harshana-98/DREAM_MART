72caa684d4854f6f8a1e4cba4ed0237dec697f07
Abrar-jinnah <abrarjinnah@gmail.com>
2020-10-01 19:25:11 +0530

    search produt activity layout

---
+++ app/src/main/java/com/example/dreammart/activity_search_products.java
@@ -0,0 +1,14 @@
+package com.example.dreammart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_search_products extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_products);
+    }
+}
