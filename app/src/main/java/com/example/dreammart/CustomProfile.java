c45f906e66f3f67e14b66d54368894cfe7e5237d
Harshana-98 <68683395+Harshana-98@users.noreply.github.com>
2020-10-02 20:11:11 +0530

    Update the all project

---
+++ app/src/main/java/com/example/dreammart/CustomProfile.java
@@ -0,0 +1,14 @@
+package com.example.dreammart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_profile);
+    }
+}
