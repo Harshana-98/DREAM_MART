9608364ed817d3b38b8f8dbf6ca02145571f863d
AshenGit <ashen6190@gmail.com>
2020-10-02 14:35:20 +0530

    Product Details layout(1)

--- app/src/main/java/com/example/dreammart/ProductDetailsActivity.java
+++ app/src/main/java/com/example/dreammart/ProductDetailsActivity.java
@@ -1,14 +1,44 @@
-package com.example.dreammart;
+package com.example.onlineshopping;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
+import Model.Products;
+import Prevalent.Prevalent;
+
+import com.example.dreammart.R;
+
+
 
 public class ProductDetailsActivity extends AppCompatActivity {
+    private Button addToCartButton;
+    private ImageView productImage;
+    private ElegantNumberButton numberButton;
+    private TextView productPrice, productDescription, productName;
+    private String productID = "", state = "Normal";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        productID = getIntent().getStringExtra("pid");
+        addToCartButton = (Button) findViewById(R.id.pd_add_to_cart_button);
+        numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
+        productImage = (ImageView) findViewById(R.id.product_image_details);
+        productName = (TextView) findViewById(R.id.product_name_details);
+        productDescription = (TextView) findViewById(R.id.product_description_details);
+        productPrice = (TextView) findViewById(R.id.product_price_details);
+
+
+
     }
 }
